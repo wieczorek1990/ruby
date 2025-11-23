fc7180a0f37ef057686263a89b77302df78069bd
Łukasz Wieczorek <luke@soiree.tech>
2025-11-23 11:29:50 +0100

    Install scripts.

---
+++ tool/install/brew_install.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+# Installs Ruby into custom /opt/rubies/ prefix.
+
+set -e
+
+hash brew
+if [ $? -ne 0 ]
+then
+ echo "Brew not found."
+ exit 1
+fi
+
+if [ $# -eq 1 ]
+then
+ build_name=$1
+ prefix=/opt/rubies/${build_name}
+ export BASERUBY=$(brew --prefix ruby)/bin/ruby
+else
+ echo "Please name the ruby build with the first program argument."
+ exit 1
+fi
+
+echo "Installing dependencies..."
+brew install autoconf openssl libyaml ruby
+echo "Done installing dependencies."
+
+echo "Starting build..."
+./autogen.sh
+mkdir -p build/
+cd build
+../configure\
+ --config-cache\
+ --prefix=${prefix}\
+ --with-baseruby\
+ --with-libyaml-dir=$(brew --prefix libyaml)\
+ --with-openssl-dir=$(brew --prefix openssl)
+make
+make install
+cd -
+echo "Done."
